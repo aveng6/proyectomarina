90330794b9a4a43fdf894cd1d3030b0943c92644
Santiago <santi1412w@gmail.com>
2024-12-04 15:38:12 -0700

    requirements.txt

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -e errexit
-pip install -r requirement.txt
+set -e  # Esto habilita el modo de salida inmediata en caso de error
+pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
