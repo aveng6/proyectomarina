1bde5a9c551d277b6846fcf038720a515d50b0ef
Santiago <santi1412w@gmail.com>
2024-12-04 16:18:36 -0700

    Resuelto conflicto en build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 set -e  # Habilita el modo de salida inmediata en caso de error
 
 # Instala las dependencias
@@ -9,10 +8,3 @@ pip install --no-cache-dir -r requirements.txt
 # Ejecuta las tareas de Django
 python manage.py collectstatic --noinput
 python manage.py migrate
-=======
-set -0 errexit
-pip install -r requirement.txt
-
-python manage.py collectstatic --noinput
-python mange.py migrate
->>>>>>> 2c99c4146a9aa150214c13d6e3b25bc59290cb01
